0097ca1ef77336045b68ada66d93433868e8e6b8
vikash <khowalvikash@gmail.com>
2024-08-14 08:56:49 -0700

    first Commit

--- javascript questions/question2.js
+++
@@ -1,6 +0,0 @@
-//Q.2 Write a function to add two numbers and return the result.
-
-function add(a,b){
-    return a+b;
-}
-console.log(add(10, 5));
--- javascript questions/question3.js
+++
@@ -1,7 +0,0 @@
-//Q.3 Create a function to calculate the area of a rectangle given its width and height.
-
-let area =(width, height) => {
-    return width*height;
-}
-let Area = area(10,5);
-console.log(`the area of rectangle is ${Area}`);
